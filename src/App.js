0612c3dc55fad301129319e4ee7d124036eb1490
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2020-10-29 14:31:20 -0300

    desafioreact

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(res =>{
+      setRepos(res.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
     // TODO
+
+    const res = await api.post('repositories', {
+      title: "Desafio Node.js", 
+      url: "http://github.com/...", 
+      techs: [
+		  "Node.js", 
+		  "..."
+		]
+    })
+
+    setRepos([...repos, res.data])
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    await api.delete(`repositories/${id}`)
+
+    setRepos(repos.filter(repo => repo.id != id))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repos.map( repo  => 
+        <li key={repo.id}>
+          {repo.title} 
+        <button onClick={() => handleRemoveRepository(repo.id)}> Remover </button>
+         </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
